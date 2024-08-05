41eb12f4e9da975c34d6409abf19eeabf9b17022
Rushikesh Nimkar <rushikeshnimkar396@gmail.com>
2024-08-05 17:33:35 +0530

    build fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Blog1 from "./pages/blogbuster1/page";
 import "./globals.css";
 import Blog2 from "./pages/blogbuster2/page";
 import Blog3 from "./pages/blogbuster3/page";
-import Contract from "./pages/contract/page";
 import { useEffect } from "react";
 import Cookies from "js-cookie";
 
@@ -21,76 +20,15 @@ const Home = () => {
 
     if (code) {
       localStorage.setItem("code", code);
-      exchangeCodeForToken(code);
       console.log("code", code);
     }
   };
 
-  const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID_GOOGLE_WEB2;
-  const REDIRECT_URI = process.env.NEXT_PUBLIC_REDIRECT_URI_GOOGLE_WEB2;
-  const CLIENT_SECRET = process.env.NEXT_PUBLIC_CLIENT_SECRET_GOOGLE_WEB2;
-
-  const exchangeCodeForToken = async (code: string) => {
-    const tokenEndpoint = 'https://www.googleapis.com/oauth2/v4/token';
-
-    const tokenRequestBody = {
-      code,
-      client_id: CLIENT_ID,
-      client_secret: CLIENT_SECRET,
-      redirect_uri: REDIRECT_URI,
-      grant_type: 'authorization_code',
-    };
-
-    try {
-      const response = await fetch(tokenEndpoint, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: new URLSearchParams(tokenRequestBody).toString(),
-      });
-
-      const tokenData = await response.json();
-
-      // Assuming id_token is present in tokenData
-      const idToken = tokenData.id_token;
-      const accessToken = tokenData.access_token;
-      sendIdToken(idToken);
-
-      console.log("token", tokenData);
-    } catch (error) {
-      console.error('Token exchange error:', error);
-    }
-  };
-
-  const sendIdToken = async (idToken: string) => {
-    try {
-      const REACT_APP_GATEWAY_URL = process.env.NEXT_PUBLIC_GATEWAY_URL;
-      const response = await fetch(`${REACT_APP_GATEWAY_URL}login`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ idToken }),
-      });
-
-      const data = await response.json();
-      console.log("Response from server:", data);
-      const token = data?.payload.token;
-      const userId = data?.payload.userId;
-      console.log("token", token);
-      Cookies.set("erebrus_token", token, { expires: 7 });
-      Cookies.set("erebrus_userid", userId, { expires: 7 });
-      window.location.reload();
-    } catch (error) {
-      console.error('Error sending idToken:', error);
-    }
-  };
 
   return (
     <main className="h-full bg-gradient-custom">
       <div className="h-[20vh]">
-        <p className="text-white text-3xl pt-7 ml-10 font-bold">Netsepio Demo</p>
+        <p className="text-white text-3xl pt-7 ml-10 font-bold">Blog Buster</p>
         <NavbarDemo />
       </div>
       <div className="flex">
